633ec7b0ea388aa15b700dcbe6eba78fcd25d51f
Md. Abrar Siddiqui <123486091+Abraralvi2002@users.noreply.github.com>
2024-09-25 21:53:59 +0600

    Create Assignment_2_Abrar.java

---
+++ Assignment_2_Abrar.java
@@ -0,0 +1,18 @@
+public class Product {
+    
+    public static void main(String[] args) {
+        int id = 101;
+        String title, price, description, category;
+
+        title = "iphone15";
+        price = "1895 euros";
+        description = "perfect product with best image quality";
+        category = " phone";
+
+        System.out.println(id);
+        System.out.println(title);
+        System.out.println(price);
+        System.out.println(description);
+        System.out.println(category);
+    }
+}
